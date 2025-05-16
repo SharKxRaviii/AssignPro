9f0f3c9128050c7a937a027c23441209b3e06fcd
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-17 02:15:26 +0530

    errorHandlerMiddleware added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-
+import { errorHandlerMiddleware } from './src/middlewares/errorHandler.middleware.js';
 // implement cors
 const corsOptions = {
     origin: 'http://localhost:5173',
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
     res.send('welcome');
 });
 
-
+app.use(errorHandlerMiddleware);
 export default app;

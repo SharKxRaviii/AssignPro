5339ea03ad362b354905e32d630e18ee30c3847b
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-17 01:54:34 +0530

    signIn method added inside userRepo

--- backend/src/features/user/user.repository.js
+++ backend/src/features/user/user.repository.js
@@ -1,20 +1,64 @@
 import userModel from "./user.schema.js";
+import bcrypt from "bcrypt";
 
 export const signUp = async (name, email, password) => {
   try {
-    const newUser = new userModel({name, email, password});
+    // create or add new user to the db for register
+    const newUser = new userModel({ name, email, password });
     const savedUser = await newUser.save();
     return {
-        success: true,
-        res: savedUser
-    }
+      success: true,
+      res: savedUser,
+    };
   } catch (error) {
     return {
+      success: false,
+      error: {
+        statusCode: 500,
+        msg: error.message,
+      },
+    };
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    // find user by email and exclude password to make the user credentials more secure
+    const user = await userModel.findOne({ email }).select("-password");
+    if (!user) {
+      return {
         success: false,
         error: {
-            statusCode: 500,
-            msg: error.message
-        }
+          statusCode: 404,
+          msg: "User not found",
+        },
+      };
     }
+
+    // compare the current password with the user password stored in db
+    const comparedPass = await bcrypt.compare(password, user.password);
+    if (!comparedPass) {
+      return {
+        success: false,
+        error: {
+          statusCode: 401,
+          msg: "Invalid credentials",
+        },
+      };
+    }
+
+    // if compared password is correct then send response
+    return {
+      success: true,
+      res: user,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: {
+        statusCode: 500,
+        msg: error.message,
+      },
+    };
   }
 };

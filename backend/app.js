0a2390952143d8f1f8c29c8db8067393a47fded9
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-17 00:31:32 +0530

    implement cors on backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
+
+// implement cors
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    method: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization'
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('welcome');

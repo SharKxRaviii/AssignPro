b239309ce257627072c9c5304873d60c9ca32d65
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-17 12:28:38 +0530

    agent repo added

---
+++ backend/src/features/agent/agent.repository.js
@@ -0,0 +1,22 @@
+import agentModel from "./agent.schema.js";
+import bcrypt from 'bcrypt';
+
+export const addAgents = async (name, email, mobile, password) => {
+    try {
+        const hashedPass = await bcrypt.hash(password, 12);
+        const newAgent = new agentModel({name, email, mobile, password: hashedPass});
+        const savedAgent = await newAgent.save();
+        return {
+            success: true,
+            res: savedAgent
+        }
+    } catch (error) {
+        return {
+            success: false,
+            error: {
+                statusCode: 500,
+                msg: error.message
+            }
+        }
+    }
+}

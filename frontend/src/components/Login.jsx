7071ac619622fb5f10f5934d1d4df158670334e1
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-18 00:47:13 +0530

    login form created

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,32 @@
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
     return(
         <div>
-            <h1>this is login page</h1>
+            <h1>Login</h1>
+            <form action="/">
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    required='true'
+                    />
+                </div>
+                
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password"
+                    id="password"
+                    placeholder="Enter your password"
+                    required='true'
+                    />
+                </div>
+
+                <button>Login</button>
+            </form>
+            <p>Create an account <NavLink to={'/signup'} className='underline'>Sign Up</NavLink>
+            </p>
         </div>
     )
 }

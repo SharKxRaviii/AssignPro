961faf59fb9a7e48c9fc7ff2a50d9839b240fb85
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-18 16:15:00 +0530

    fileValidation added

---
+++ backend/src/middlewares/fileValidator.middleware.js
@@ -0,0 +1,19 @@
+const allowedMimeTypes = [
+    'text/csv',
+    'application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+];
+
+export default function fileValidator(req, res, next) {
+    const file = req.file;
+
+    if(!file) {
+        return res.status(400).json({message: "no file uploaded"});
+    }
+
+    if(!allowedMimeTypes.includes(file.mimeTypes)){
+        return res.status(400).json({ message: 'Invalid file type. Only CSV, XLS, and XLSX are allowed.' });
+    }
+
+    next();
+} 

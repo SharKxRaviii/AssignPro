739945851638248148c533dc171babf92fd799f5
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-18 22:10:36 +0530

    change the upload controller route import

--- backend/src/features/upload/upload.routes.js
+++ backend/src/features/upload/upload.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import uploadFile from '../../middlewares/fileUploads.middleware.js';
 import fileValidator from '../../middlewares/fileValidator.middleware.js';
-import { handleFileUpload } from './controller.js';
+import { handleFileUpload } from './upload.controller.js';
 
 const uploadRoutes = express.Router();
 

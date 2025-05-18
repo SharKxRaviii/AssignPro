bd427d99dbdc2c3e74bc7224a1aa1c75989a62a2
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-18 12:44:34 +0530

    NavBar styling, Login & Signup Form added with styling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,36 +1,83 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return(
-        <div>
-            <h1>Login</h1>
-            <form action="/">
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email"
-                    id="email"
-                    placeholder="Enter your email"
-                    name="email"
-                    required
-                    />
-                </div>
-                
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password"
-                    id="password"
-                    placeholder="Enter your password"
-                    name="password"
-                    required
-                    />
-                </div>
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-16 w-auto"
+          src="/images/logo.png"
+          alt="Company Logo"
+        />
+        <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Login to your account
+        </h2>
+      </div>
 
-                <button>Login</button>
-            </form>
-            <p>Create an account <NavLink to={'/signup'} className='underline'>Sign Up</NavLink>
-            </p>
-        </div>
-    )
-}
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autocomplete="email"
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                for="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                autocomplete="current-password"
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm/6 text-gray-500">
+          Create new account {""}
+          <Link
+            to={"/signup"}
+            className="font-semibold text-indigo-600 hover:text-indigo-500"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,20 +2,31 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-  <div>
-    <ul>
-      <li><NavLink to="/">LOGO</NavLink></li>
-    </ul>
-  </div>
-  <div>
-    <ul>
-      <li><NavLink to="/login">Login</NavLink></li>
-      <li><NavLink to="/signup">Sign Up</NavLink></li>
-    </ul>
-  </div>
-</nav>
-
+    <nav className="w-full flex items-center justify-evenly bg-indigo-900 px-8 py-2">
+      <div>
+        <NavLink to="/">
+          <img
+            className="h-16 w-auto"
+            src="/images/logo.png"
+            alt="Company Logo"
+          />
+        </NavLink>
+      </div>
+      <div>
+        <ul className="flex gap-x-6">
+          <li>
+            <NavLink to="/login" className="text-white text-lg hover:underline">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup" className="text-white text-lg hover:underline">
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 

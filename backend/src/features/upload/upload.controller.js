5d5dead7f40dec4ed44f0e2323dcb46164a186ee
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-18 20:59:30 +0530

    handleFileUpload controller added

---
+++ backend/src/features/upload/upload.controller.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import { saveAssignedLeads } from './upload.repository.js';
+import { parseCSV, parseExcel } from '../../file_parser/fileParser.js';
+import agentModel from '../agent/agent.schema.js';
+import ErrorHandler from '../../middlewares/errorHandler.middleware.js';
+
+export const handleFileUpload = async (req, res) => {
+    try {
+        const file = req.file;
+        if(!file) {
+            return res.status(400).json({message: 'No file uploaded'});
+        }
+
+        const ext = path.extname(file.originalname).toLowerCase();
+        if(!['.csv', '.xlsx', '.xls'].includes(ext)) {
+            return res.status(400).json({message: 'Invalid file type'});
+        }
+
+        let parsedData = ext === '.csv' ? await parseCSV(file.path) : parseExcel(file.path);
+
+        const validData = parsedData.filter((item) => item.firstName && item.phone && item.notes);
+        if(validData.length === 0) {
+            return res.status(400).json({message: 'No valid entries found'});
+        }
+
+        const agents = await agentModel.find().limit(5);
+        if(agents.length < 1) {
+            return res.status(400).json({message: 'No agents found'});
+        }
+
+        let agentIndex = 0;
+        const distributedData = validData.map((entry) => {
+            const agent = agents[agentIndex];
+            agentIndex = (agentIndex + 1) % agents.length;
+
+            return {
+                firstName: entry.firstName,
+                phone: entry.phone,
+                notes: entry.notes,
+                assignedTo: agent._id
+            }
+        });
+
+        await saveAssignedLeads(distributedData);
+
+        res.status(201).json({message: 'Files uploaded and assigned successfully'});
+    } catch (error) {
+        throw new ErrorHandler("Server Error, try again later!!", 500);
+    }
+}  

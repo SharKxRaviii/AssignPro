cd7441cba74c838e4edf3308d043a9a6724f97fc
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-18 16:00:27 +0530

    fileUpload middleware added

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col justify-center px-6 py-10 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-16 w-auto"
